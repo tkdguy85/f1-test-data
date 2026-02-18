95698556c92ea4b8174ad8809110e1346e22994f
Dallas3 <dallasgordon3@gmail.com>
2026-02-17 18:26:25 -0800

    Claude generated layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import { LineGraph } from './components/Line.jsx'
+import F1Dashboard from './components/F1Dashboard.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1>F1 Test Data</h1>
-      <LineGraph />
+      {/* <LineGraph /> */}
+      <F1Dashboard />
     </>
   )
 }
