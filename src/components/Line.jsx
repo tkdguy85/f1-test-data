1e0557d02e87961ef9cd564cb910201aa3f54c03
Dallas3 <dallasgordon3@gmail.com>
2026-02-17 17:37:31 -0800

    Added dummy data and results JSON for testing runs

---
+++ src/components/Line.jsx
@@ -0,0 +1,29 @@
+import { Line } from 'react-chartjs-2'
+import teamData from '../data/SAMPLE_DATA'
+import { 
+  Chart as ChartJS, 
+  CategoryScale, 
+  LinearScale, 
+  PointElement, 
+  LineElement, 
+  Title, 
+  Tooltip, 
+  Legend
+} from 'chart.js'
+
+// Needed to render the graphs
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const LineGraph = () => {
+  const options = {}
+  
+  return <Line options={options} data={teamData}/>
+}
---
+++ src/data/SAMPLE_DATA.js
@@ -0,0 +1,32 @@
+const teamData = {
+  labels: [
+    'Mercedes',
+    'Mclaren',
+    'Red Bull',
+    'VCARB',
+    'Ferrari',
+    'Alpine',
+    'Cadillac',
+    'Aston Martin',
+    'Haas',
+    'Williams',
+    'Audi'
+  ],
+  datasets: [
+    {
+      label: 'Testing Lap Times Day 1',
+      data: [1.19, 1.20, 1.21, 1.22, 1.23, 1.24, 1.25, 1.26, 1.27, 1.28, 1.29],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    },
+        {
+      label: 'Testing Lap Times Day 2',
+      data: [1.18, 1.18, 1.19, 1.26, 1.19, 1.22, 1.21, 1.31, 1.23, 1.21, 1.22],
+      borderColor: 'rgb(219, 255, 99)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    }
+
+  ]
+}
+
+export default teamData
