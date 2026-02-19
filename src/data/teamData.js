b4cae3a5a576663b0e53f3ccd1a9712f5b6d0f45
Dallas3 <dallasgordon3@gmail.com>
2026-02-19 12:21:30 -0800

    Team data added

---
+++ src/data/teamData.js
@@ -0,0 +1,189 @@
+export const teamData = [
+  {
+    name: "Alpine",
+    code: "ALP",
+    color: "#479fe2",
+    engine: "Mercedes-AMG F1 M17",
+    chassis: "A526",
+    drivers: [
+      { 
+        name: "Pierre Gasly", 
+        number: 10 
+      },
+      { 
+        name: "Franco Colapinto", 
+        number: 43 
+      }
+    ]
+  },
+  {
+    name: "Aston Martin Honda",
+    code: "AST",
+    color: "#4b9774",
+    engine: "Honda RA626H",
+    chassis: "AMR26",
+    drivers: [
+      { 
+        name: "Fernando Alonso", 
+        number: 14 
+      },
+      { 
+        name: "Lance Stroll", 
+        number: 18 
+      }
+    ]
+  },
+  {
+    name: "Atlassian Williams",
+    code: "WIL",
+    color: "#3267d4",
+    engine: "Mercedes-AMG F1 M17",
+    chassis: "FW48",
+    drivers: [
+      { 
+        name: "Carlos Sainz", 
+        number: 55 
+      },
+      { 
+        name: "Alexander Albon", 
+        number: 23 
+      }
+    ]
+  },
+  {
+    name: "Audi Revolut",
+    code: "AUD",
+    color: "#eb4526",
+    engine: "Audi AFR 26 Hybrid",
+    chassis: "R26",
+    drivers: [
+      { 
+        name: "Niko Hulkenberg", 
+        number: 27 
+      },
+      { 
+        name: "Gabriel Bortoleto", 
+        number: 5 }
+        
+    ]
+  },
+  {
+    name: "Cadillac",
+    code: "CAD",
+    color: "#aaaadd",
+    engine: "Ferrari 067/6",
+    chassis: "CA01",
+    drivers: [
+      { 
+        name: "Sergio Perez", 
+        number: 11 
+      },
+      { 
+        name: "Valterri Bottas", 
+        number: 77 
+      }
+    ]
+  },
+  {
+    name: "Scuderia Ferrari",
+    code: "FER",
+    color: "#b52e37",
+    engine: "Ferrari 067/6",
+    chassis: "SF26",
+    drivers: [
+      { 
+        name: "Lewis Hamilton", 
+        number: 44 
+      },
+      { 
+        name: "Charles LeClerc", 
+        number: 16 
+      }
+    ]
+  },
+  {
+    name: "Haas",
+    code: "HAS",
+    color: "#dfe1e2",
+    engine: "Ferrari 067/6",
+    chassis: "VF26",
+    drivers: [
+      { 
+        name: "Esteban Ocon", 
+        number: 31 
+      },
+      { 
+        name: "Oliver Bearman", 
+        number: 87 
+      }
+    ]
+  },
+  {
+    name: "McLaren",
+    code: "MCL",
+    color: "#ef8733",
+    engine: "Mercedes-AMG F1 M17",
+    chassis: "MCL40",
+    drivers: [
+      { 
+        name: "Lando Norris", 
+        number: 1 }
+        ,
+      { 
+        name: "Oscar Piastri", 
+        number: 81 
+      }
+    ]
+  },
+  {
+    name: "Mercedes",
+    code: "MER",
+    color: "#75f1d3",
+    engine: "Mercedes-AMG F1 M17",
+    chassis: "F1W17",
+    drivers: [
+      { 
+        name: "George Russell", 
+        number: 63 
+      },
+      { 
+        name: "Kimi Antonelli", 
+        number: 12 
+      }
+    ]
+  },
+  {
+    name: "Racing Bulls",
+    code: "VCARB",
+    color: "#7091f8",
+    engine: "Red Bull Ford DM01",
+    chassis: "VCARB03",
+    drivers: [
+      { 
+        name: "Liam Lawson", 
+        number: 30 
+      },
+      { 
+        name: "Arvid Lindblad", 
+        number: 41 
+      }
+    ]
+  },
+  {
+    name: "Red Bull",
+    code: "RBR",
+    color: "#4570c0",
+    engine: "Red Bull Ford DM01",
+    chassis: "RB22",
+    drivers: [
+      { 
+        name: "Max Verstappen", 
+        number: 3 }
+        ,
+      { 
+        name: "Isack Hadjar", 
+        number: 6 }
+        
+    ]
+  }
+]
