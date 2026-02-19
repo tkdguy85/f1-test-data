f43dce935dc803cda760308bce6d1986e38d768e
Dallas3 <dallasgordon3@gmail.com>
2026-02-19 13:02:11 -0800

    Run summary updated and old files moved

---
+++ src/data/runSummary.js
@@ -0,0 +1,206 @@
+export const day1RunSummary = [
+  {
+    team: "Williams",
+    drivers: ["Sainz", "Albon"],
+    laps: 145,
+    kilometres: 785
+  },
+  {
+    team: "Red Bull",
+    drivers: ["Verstappen"],
+    laps: 136,
+    kilometres: 736
+  },
+  {
+    team: "Ferrari",
+    drivers: ["Hamilton", "Leclerc"],
+    laps: 132,
+    kilometres: 714
+  },
+  {
+    team: "Audi",
+    drivers: ["Bortoleto", "Hulkenberg"],
+    laps: 122,
+    kilometres: 660
+  },
+  {
+    team: "Haas",
+    drivers: ["Ocon"],
+    laps: 115,
+    kilometres: 622
+  },
+  {
+    team: "McLaren",
+    drivers: ["Piastri", "Norris"],
+    laps: 112,
+    kilometres: 606
+  },
+  {
+    team: "Cadillac",
+    drivers: ["Bottas", "Perez"],
+    laps: 107,
+    kilometres: 579
+  },
+  {
+    team: "Mercedes",
+    drivers: ["Russell", "Antonelli"],
+    laps: 86,
+    kilometres: 465
+  },
+  {
+    team: "Alpine",
+    drivers: ["Colapinto", "Gasly"],
+    laps: 77,
+    kilometres: 416
+  },
+  {
+    team: "Racing Bulls",
+    drivers: ["Lindblad"],
+    laps: 75,
+    kilometres: 406
+  },
+  {
+    team: "Aston Martin",
+    drivers: ["Stroll"],
+    laps: 36,
+    kilometres: 195
+  }
+]
+
+export const day2RunSummary = [
+  {
+    team: "McLaren", 
+    drivers: ["Norris"], 
+    laps: 149, 
+    kilometres: 806 
+  },
+  {
+    team: "Ferrari", 
+    drivers: ["Leclerc"], 
+    laps: 139, 
+    kilometres: 752 
+  },
+  {
+    team: "Racing Bulls", 
+    drivers: ["Lawson", "Lindblad"], 
+    laps: 133, 
+    kilometres: 720 
+  },
+  {
+    team: "Williams", 
+    drivers: ["Albon", "Sainz"], 
+    laps: 131, 
+    kilometres: 709 
+  },
+  {
+    team: "Haas", 
+    drivers: ["Bearman"], 
+    laps: 130, 
+    kilometres: 704 
+  },
+  {
+    team: "Audi", 
+    drivers: ["Hulkenberg", "Bortoleto"], 
+    laps: 114, 
+    kilometres: 617 
+  },
+  {
+    team: "Cadillac", 
+    drivers: ["Perez", "Bottas"], 
+    laps: 109, 
+    kilometres: 590 
+  },
+  {
+    team: "Aston Martin", 
+    drivers: ["Alonso"], 
+    laps: 98, 
+    kilometres: 530 
+  },
+  {
+    team: "Alpine", 
+    drivers: ["Gasly"], 
+    laps: 97, 
+    kilometres: 525 
+  },
+  {
+    team: "Red Bull", 
+    drivers: ["Hadjar"], 
+    laps: 87, 
+    kilometres: 471 
+  },
+  {
+    team: "Mercedes", 
+    drivers: ["Antonelli", "Russell"], 
+    laps: 57, 
+    kilometres: 309 
+  }
+]
+
+export const day3RunSummary = [
+  {
+    team: "McLaren", 
+    drivers: ["Piastri"], 
+    laps: 161, 
+    kilometres: 871 
+  },
+  {
+    team: "Ferrari", 
+    drivers: ["Hamilton"], 
+    laps: 150, 
+    kilometres: 812 
+  },
+  {
+    team: "Williams", 
+    drivers: ["Sainz", "Albon"], 
+    laps: 146, 
+    kilometres: 790 
+  },
+  {
+    team: "Haas", 
+    drivers: ["Bearman", "Ocon"], 
+    laps: 145, 
+    kilometres: 785 
+  },
+  {
+    team: "Alpine", 
+    drivers: ["Colapinto"], 
+    laps: 144, 
+    kilometres: 779 
+  },
+  {
+    team: "Mercedes", 
+    drivers: ["Russell", "Antonelli"], 
+    laps: 139, 
+    kilometres: 752 
+  },
+  {
+    team: "Red Bull", 
+    drivers: ["Verstappen", "Hadjar"], 
+    laps: 120, 
+    kilometres: 649 
+  },
+  {
+    team: "Racing Bulls", 
+    drivers: ["Lawson"], 
+    laps: 119, 
+    kilometres: 644 
+  },
+  {
+    team: "Audi", 
+    drivers: ["Bortoleto", "Hulkenberg"], 
+    laps: 118, 
+    kilometres: 639 
+  },
+  {
+    team: "Cadillac", 
+    drivers: ["Bottas", "Perez"], 
+    laps: 104, 
+    kilometres: 563 
+  },
+  {
+    team: "Aston Martin", 
+    drivers: ["Stroll"], 
+    laps: 72, 
+    kilometres: 390 
+  }
+]
